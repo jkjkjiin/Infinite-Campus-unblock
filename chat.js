23ac0fcf29cb38036d7bf6cf398f3d5792747e31
Hacker41 <199835595+Ryann-4@users.noreply.github.com>
2025-12-05 13:55:38 -0600

    fixed chat

--- chat.js
+++ chat.js
@@ -1,4 +1,4 @@
-import { auth, db } from "./firebase.js";
+import { auth, db } from "./chatfirebase.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-auth.js";
 import {
     ref, push, onChildAdded, onChildRemoved, onChildChanged,
